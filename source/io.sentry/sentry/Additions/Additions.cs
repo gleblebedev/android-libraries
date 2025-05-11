db946843d34f05d88cf6460a491a817ed00856fa
Gleb <gleb@gleblebedev.com>
2025-05-11 19:43:42 +0100

    io.sentry\sentry fix

---
+++ source/io.sentry/sentry/Additions/Additions.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace IO.Sentry
+{
+	public partial class SentryInstantDate
+	{
+		public override int CompareTo (global::Java.Lang.Object other)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+	public partial class SentryLongDate
+	{
+		public override int CompareTo (global::Java.Lang.Object other)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+	public partial class SentryNanotimeDate
+	{
+		public override int CompareTo (global::Java.Lang.Object other)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
